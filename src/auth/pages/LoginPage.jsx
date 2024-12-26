0a1cb05cdae4c174b60202dd58e89fe0aa8ecc58
Jhonny <jhonnycgarcia@gmail.com>
2024-12-26 12:22:08 -0500

    LoginPage - Diseño - Segunda Parte

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid2, TextField, Typography } from "@mui/material"
+import { Google } from "@mui/icons-material"
+import { Button, Grid2, Link, TextField, Typography } from "@mui/material"
+import { Link as RouterLink } from "react-router-dom"
 
 export const LoginPage = () => {
   return (
@@ -35,7 +37,27 @@ export const LoginPage = () => {
                 fullWidth
               />
             </Grid2>
-            
+
+            <Grid2 container size={12} spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid2>
+            </Grid2>
+
+            <Grid2 container size={12} spacing={ 2 } direction="row" justifyContent="end">
+              <Link component={RouterLink} color="inherit" to="/auth/register">
+                Crear una cuenta
+              </Link>
+            </Grid2>
+
           </Grid2>
         </form>
 
