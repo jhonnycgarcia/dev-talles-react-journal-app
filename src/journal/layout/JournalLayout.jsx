88f4d601b8d569db4aa3fac921aa786b737d049a
Jhonny <jhonnycgarcia@gmail.com>
2024-12-26 21:09:40 -0500

    SideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,25 +1,19 @@
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from '../components';
 import PropTypes from 'prop-types';
-import { Box } from "@mui/material"
-import { NavBar } from '../components/NavBar';
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-
-        {/* Navbar drawerWidth */}
         <NavBar drawerWidth={ drawerWidth } />
-
-        {/* Sidebar drawerWidth */}
-
+        <SideBar drawerWidth={ drawerWidth } />
         <Box 
-            
             component="main"
             sx={{ flexGrow: 1, p: 3 }}
         >
-            {/* Toolbar */}
-
+            <Toolbar />
             { children }
         </Box>
     </Box>
