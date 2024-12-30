759d88ae0e426cd257843b9c9b73a502344614d5
Jhonny <jhonnycgarcia@gmail.com>
2024-12-29 22:08:59 -0500

    Pruebas con los Slices de Redux Toolkit

---
+++ tests/fixtures/authFixtures.js
@@ -0,0 +1,37 @@
+
+export const initialState = {
+    status: 'checking',
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
+    providerId: null,
+}
+
+export const authenticatedState = {
+    status: 'authenticated',
+    uid: '123',
+    email: 'example@email.com',
+    displayName: 'Example User',
+    photoURL: 'https://dummyimage.com/200x200/000/fff',
+    errorMessage: null,
+    providerId: 'firebase',
+}
+
+export const notAuthenticatedState = {
+    status: 'not-authenticated',
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
+    providerId: null,
+}
+
+export const demoUser = {
+    uid: '123',
+    email: 'example@email.com',
+    displayName: 'Example User',
+    photoURL: 'https://dummyimage.com/200x200/000/fff',
+}
