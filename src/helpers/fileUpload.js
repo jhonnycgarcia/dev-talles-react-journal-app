4fa51b4012e2c837baca81fc659cca97628b5c53
Jhonny <jhonnycgarcia@gmail.com>
2024-12-30 21:15:24 -0500

    feat(chore): Se implementa lectura de variable de entorno en helper para realizar carga de imagen a cloudinary

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,11 +1,13 @@
+import { getEnvironments } from "./getEnvironments";
+
+const { VITE_CLOUDINARY_CLOUD_NAME, VITE_CLOUDINARY_UPLOAD_PRESET } = getEnvironments();
 
 export const fileUpload = async (file) => {
-    // if(!file) { throw new Error('File is required'); }
     if(!file) { return null; }
     
-    const cloudUrl = 'https://api.cloudinary.com/v1_1/jhonnycgarcia/image/upload';
+    const cloudUrl = `https://api.cloudinary.com/v1_1/${VITE_CLOUDINARY_CLOUD_NAME}/image/upload`;
     const formData = new FormData();
-    formData.append('upload_preset', 'dev-talles-react-journal-app');
+    formData.append('upload_preset', VITE_CLOUDINARY_UPLOAD_PRESET);
     formData.append('file', file);
 
     try {
