25957a9d4fc227a670aceb6869bc525d6fbba07d
Jhonny <jhonnycgarcia@gmail.com>
2024-12-30 22:31:22 -0500

    Pruebas en el LoginPage

---
+++ tests/auth/pages/LoginPage.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { LoginPage } from "../../../src/auth/pages/LoginPage";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "../../../src/store";
+import { MemoryRouter } from "react-router-dom";
+
+const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer
+    },
+    // preloadedState: {
+
+    // }
+});
+
+describe('Pruebas en <LoginPage />', () => {
+    
+    test('debe de mostrar el componente correctamente ', () => {
+        render(
+            <Provider store={ store }>
+                <MemoryRouter
+                    future={{v7_startTransition: true, v7_relativeSplatPath: true }}
+                >
+                    <LoginPage />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        // screen.debug();
+        expect( screen.getAllByText('Login').length ).toBeGreaterThanOrEqual(1);
+    });
+
+});
