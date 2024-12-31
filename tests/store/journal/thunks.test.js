e668d25dc5980c82647edce8f0d3f571ab851a79
Jhonny <jhonnycgarcia@gmail.com>
2024-12-30 20:41:19 -0500

    Crear base de datos de testing + Prueba completa sobre inserción

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,5 +1,20 @@
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { addNewEmptyNote, setActiveNote, setSaving } from "../../../src/store/journal/journalSlice";
 import { startNewNote } from "../../../src/store/journal/thunks";
 import { uid as mockUid } from "../../fixtures/authFixtures";
+import { FirebaseFirestore } from "../../../src/firebase";
+
+const clearCollections = async () => {
+    const collectionRef = collection(FirebaseFirestore, `${mockUid}/journal/notes`);
+    const docs = await getDocs(collectionRef);
+
+    const deletePromises = [];
+    docs.forEach(doc => {
+        deletePromises.push(deleteDoc(doc.ref));
+    });
+
+    await Promise.all(deletePromises);
+}
 
 describe('Pruebas en JournalThunks', () => {
 
@@ -11,8 +26,25 @@ describe('Pruebas en JournalThunks', () => {
     });
     
     test('debe de crear una nueva nota en blanco', async () => {
-        getState.mockReturnValue({ auth: { uid: mockUid }})
+        getState.mockReturnValue({ auth: { uid: mockUid }});
+
         await startNewNote()(dispatch, getState);
+
+        const note = {
+            title: '',
+            body: '',
+            date: expect.any(Number),
+            id: expect.any(String),
+            imageUrls: []
+        };
+
+        expect(dispatch).toHaveBeenCalledTimes(3);
+        expect(dispatch).toHaveBeenCalledWith(setSaving());
+        expect(dispatch).toHaveBeenCalledWith(addNewEmptyNote(note));
+        expect(dispatch).toHaveBeenCalledWith(setActiveNote(note));
+
+        // Borrar de firestore
+        await clearCollections();
     });
 
 });
